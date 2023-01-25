ec756dc0e20095d5d59c29b3e245b581cde0945b
Marco <macall30@yahoo.com>
2023-01-25 11:03:50 +0100

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">eLoro</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
